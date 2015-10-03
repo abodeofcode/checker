c00d29870a1a2f8f502263c88ff9a843013864de
abodeofcode <sumeetdceian2007@gmail.com>
2015-10-03 18:21:56 +0530

    Removed Bugs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,14 +5,14 @@ class UserMailer < ApplicationMailer
   def welcome_email
   	@order = Order.last
 
-    mail(to: @order.email, subject: 'Order on Hathi').deliver
+    mail(to: @order.email, subject: 'Your Order Details on GoHathi').deliver
   end
 
   # Send order success email to customer. We are not taking his email id
   def order_email(order)
   	@order = order
 
-  	mail(to: @order.email, subject: 'Order on Hathi').deliver
+  	mail(to: @order.email, subject: 'Your Order Details on GoHathi').deliver
 
 
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
    :port                 => 587,
    :domain               => "gmail.com",
    :user_name            => 'gohathi@gmail.com',
-   :password             => 'dhrhemsid@8888',
+   :password             => 'gohathiinspace',
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
